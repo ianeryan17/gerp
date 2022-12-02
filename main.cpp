ccece81293d533db17784690502ae73ea9df4f8a
Ryo Shimada <ryobshimada@gmail.com>
2022-12-02 17:27:15 -0500

    V3 continued

--- main.cpp
+++ main.cpp
@@ -10,12 +10,30 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <algorithm>
+#include <cstdio>
+#include <cstring>
+#include <iomanip>
+
 
 using namespace std;
 
+void printAll(DirNode* rootNode, string dirName);
+
 int main(int argc, char *argv[]){
     string dir = argv[1];
-    createPath(dir);
+    DirNode bar("Bar");
+    DirNode emptyNode("empty");
+    bar.addFile("x.cpp");
+    bar.addFile("y.cpp");
+    bar.addFile("z.cpp");
+    DirNode foo("Foo");
+    foo.addSubDirectory(&bar);
+    foo.addSubDirectory(&emptyNode);
+    foo.addFile("a.cpp");
+    foo.addFile("b.cpp");
+    foo.addFile("c.cpp");
+    printAll(&foo, dir);
 
     return 0;
 }
---
+++ main2.cpp
@@ -0,0 +1,25 @@
+/*
+ * Ryo Shimada and Ian Ryan
+ * 11/30/22
+ * 
+ * This file acts as the main for stringProcessing.cpp.
+ */
+
+#include "stringProcessing.h"
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <cstdio>
+#include <cstring>
+#include <iomanip>
+
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+    cout << stripNonAlphaNum("comp$15$69###") << endl;
+
+    return 0;
+}
